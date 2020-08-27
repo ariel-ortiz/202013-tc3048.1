72b0759187f0ba5e957304b0f72573807ddfb239
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-08-26 22:09:33 -0500

    Ejemplo de uso de expresiones regulares

---
+++ Regular Expressions/hello.c
@@ -0,0 +1,11 @@
+/* This is the classical
+ * Hello, World! program.
+ */
+
+#include <stdio.h>
+
+int main() {  /* start of program */
+    printf("Hello, World!\n");
+    return 0;
+}
+/* the end */
---
+++ Regular Expressions/sin_comentarios.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class SinComentarios {
+    public static void Main() {
+        var regex = new Regex(@"([/][*].*?[*][/])|(.)",
+                              RegexOptions.Singleline);
+        var text = File.ReadAllText("hello.c");
+        foreach (Match match in regex.Matches(text)) {
+            if (match.Groups[1].Success) {
+                // encontró comentario
+            } else {
+                Console.Write(match.Value);
+            }
+        }
+    }
+}
