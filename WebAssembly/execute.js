06c0acff1f977e7221e70c99a769898ac9f3e473
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-11-12 15:42:31 -0600

    Cambia la manera de usar promesas por async/await

--- WebAssembly/execute.js
+++ WebAssembly/execute.js
@@ -10,24 +10,28 @@ const importArgs = {
   }
 };
 
-require('wabt')().then(wabt => {
+async function getInstance() {
   const fileContent = fs.readFileSync(fileName, 'utf8');
+  const wabt = await require('wabt')();
   const wasmModule = wabt.parseWat(fileName, fileContent);
   const buffer = wasmModule.toBinary({}).buffer;
-  return WebAssembly.compile(buffer);
-})
-.then(module => {
+  const module = await WebAssembly.compile(buffer);
   return WebAssembly.instantiate(module, importArgs);
-})
-.then(instance => {
-  const { duplicate, fahrenheit2celsius, pruebita } = instance.exports;
-  console.log(duplicate(15));
-  console.log(fahrenheit2celsius(212));
-  console.log(fahrenheit2celsius(32));
-  console.log(fahrenheit2celsius(-40));
-  console.log(pruebita(10.1, 5.2));
-})
-.catch(error => {
-  console.log(error.message);
-  process.exit(1);
-});
+}
+
+async function main() {
+  try {
+    const instance = await getInstance();
+    const { duplicate, fahrenheit2celsius, pruebita } = instance.exports;
+    console.log(duplicate(15));
+    console.log(fahrenheit2celsius(212));
+    console.log(fahrenheit2celsius(32));
+    console.log(fahrenheit2celsius(-40));
+    console.log(pruebita(10.1, 5.2));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
+
